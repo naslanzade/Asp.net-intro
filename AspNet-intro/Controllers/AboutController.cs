710e0ff0d659e4e788628b4179c7dd8fd47b88bb
naslanzade <novrastafa@code.edu.az>
2023-04-29 23:34:01 +0400

    add task

---
+++ AspNet-intro/Controllers/AboutController.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNet_intro.Controllers
+{
+    public class AboutController
+    {
+
+        public string Index()
+        {
+            return "This is about page";
+        }
+    }
+}
---
+++ AspNet-intro/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet_intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            int[] nums= {1,2,3,4,5,6,7,8};
+             return View(nums);
+        }
+    }
+}
